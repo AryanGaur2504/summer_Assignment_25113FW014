77d62c021d8114e9c3608ac7243335ddc8b90bbd
AryanGaur2504 <aryangaur.2504@gmail.com>
2026-06-12 11:54:01 +0530

    Day 12 Solutions

---
+++ Day_12/Q47.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Q47{
+    //Generating nth Fibonacci Number
+    static int fib(int n){
+        if(n <= 1) return n;
+        return fib(n-1) + fib(n-2);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a < 0 || b < 0 || a > b){
+            System.out.println("Invalid Input");
+        }
+        //Looping And Printing Fibonacci Number For All i ∈ [a,b]
+        else{
+                for (int i = a; i <= b; i++) {
+                    System.out.print(fib(i) + "\t");   
+                }
+        }
+        sc.close();
+    }
+}
